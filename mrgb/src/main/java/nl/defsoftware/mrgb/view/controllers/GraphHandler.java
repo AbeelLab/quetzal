ff34149b198441b76b6e0660ea43a6323716a46d
D.L. Ettema <Daniel@defsoftware.nl>
2016-09-21 12:40:12 +0200

    initial check of a new graphhandler. also added app.log to ignore list

---
+++ mrgb/src/main/java/nl/defsoftware/mrgb/view/controllers/GraphHandler.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package nl.defsoftware.mrgb.view.controllers;
+
+import nl.defsoftware.mrgb.view.models.GraphModel;
+
+/**
+ * This GraphHandler object is responsible for filling the graph model with the nodes and their coordinates.
+ * 
+ * @author D.L. Ettema
+ *
+ */
+public class GraphHandler {
+
+	public void setGraphViewModel(GraphModel model) {
+		
+	}
+	
+}
