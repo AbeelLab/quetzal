3d956b56d90e5829249dad64eef61bd6d265e094
Daniel Ettema <daniel@defsoftware.nl>
2017-03-25 13:42:46 +0100

    Added some comment andfixed some minor loose ends. Not ready yet to be fully integrated

--- mrgb/src/main/java/nl/defsoftware/mrgb/services/LongestPathAlgorithm.java
+++ mrgb/src/main/java/nl/defsoftware/mrgb/services/LongestPathAlgorithm.java
@@ -3,11 +3,9 @@ package nl.defsoftware.mrgb.services;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Stack;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectLinkedOpenHashMap;
 import nl.defsoftware.mrgb.models.graph.Node;
@@ -19,13 +17,10 @@ import nl.defsoftware.mrgb.models.graph.Node;
  *
  */
 public class LongestPathAlgorithm {
-    
-    private Deque<Integer> stack = new LinkedList<>();
 
     /**
-     * Given a graph stored in a {@link Int2ObjectLinkedOpenHashMap} with
-     * <code><ID,Node></code> we determine the longest possible path(s). We
-     * return a array of int IDs that correspond to the IDs in the provided Map.
+     * Given a graph stored in a {@link Int2ObjectLinkedOpenHashMap} with <code><ID,Node></code> we determine the
+     * longest possible path(s). We return a array of int IDs that correspond to the IDs in the provided Map.
      * 
      * @param sequencesDataMap
      * @return
@@ -36,7 +31,6 @@ public class LongestPathAlgorithm {
         return new int[] {};
     }
 
-    
     /**
      * Dynamic programming fashion of determining the longest path by depth first search, without memoization.
      * 
@@ -49,7 +43,8 @@ public class LongestPathAlgorithm {
             return 1;
         }
 
-        //depth first search of calculating only the number of nodes, not which ones
+        // depth first search of calculating only the number of nodes, not which
+        // ones
         List<Integer> scoresOfOutNodes = new ArrayList<>();
         for (Node outNode : nodeU.getOutEdges()) {
             scoresOfOutNodes.add(calculateDFSAmountOfStepsOfLongestPath(outNode, targetNode));
@@ -72,58 +67,68 @@ public class LongestPathAlgorithm {
         }
         return scores.get(highestIndex);
     }
-    
-    //---------------------------------------------------------
-    
+
+    // --------------------------------- BFS implementation
+    // --------------------------------
+
     private Map<Integer, List<DistanceNodeTupel>> connectionFromMap = new HashMap<>();
-    //example: <1, <2,4>>
+    private Deque<Integer> stack = new LinkedList<>();
     
     /**
-     * This uses an iterative algorithm to determine the longest path in a given graph map (sequencesDataMap) and uses a Breadth First Search
+     * This uses an iterative algorithm to determine the longest path in a given graph map (sequencesDataMap) and uses a
+     * Breadth First Search. The algorithm puts the node ID's on a queue (<code>Deque</code>). From the queue it
+     * retrieves the node from the given <code>Int2ObjectLinkedOpenHashMap</code> and finds all the children of this
+     * node and puts their ID's on the queue. The algorithm maintains a <code>Map</code> with on each entry a
+     * <code>List</code> of <code>DistanceNodeTupel</code>. Each entry in the list has node ID and the distance TO this
+     * node id from the source node.
      * 
      * 
      * @param sequencesDataMap
      */
     public void findLongestPathBFS(final Int2ObjectLinkedOpenHashMap<Node> sequencesDataMap) {
         int lastFromId = 0;
-        while(!stack.isEmpty()) {
+        stack.add(0);//start with source node
+        int targetNodeId = 100;//find the targetnode id
+        while (!stack.isEmpty()) {
             lastFromId = stack.pop();
             Node fromNode = sequencesDataMap.get(lastFromId);
-            if (calcBFSLongestPathNumberOfSteps(fromNode, 1)) {
+            if (calcBFSLongestPathNumberOfSteps(fromNode, targetNodeId)) {
                 stack.clear();
             }
         }
         System.out.println("max steps: " + getMaxDistanceToThisNode(lastFromId));
-        
+
     }
-    
-    private boolean calcBFSLongestPathNumberOfSteps(final Node fromNode, int targetNodeId) {
-       if (fromNode.getNodeId() == targetNodeId) {
-           return true;
-       } else {
-           int dist = getMaxDistanceToThisNode(fromNode.getNodeId()) + 1;
-           for (Node outNode : fromNode.getOutEdges()) {
-               if (connectionFromMap.containsKey(outNode.getNodeId())) {
-                   connectionFromMap.put(outNode.getNodeId(), new ArrayList<DistanceNodeTupel>());
-               }
-               connectionFromMap.get(outNode.getNodeId()).add(new DistanceNodeTupel(fromNode.getNodeId(), dist));
-               stack.add(outNode.getNodeId());
-           }
-           return false;
-       }
+
+    private boolean calcBFSLongestPathNumberOfSteps(final Node fromNode, final int targetNodeId) {
+        if (fromNode.getNodeId() == targetNodeId) {
+            return true;
+        } else {
+            int dist = getMaxDistanceToThisNode(fromNode.getNodeId()) + 1;
+            for (Node outNode : fromNode.getOutEdges()) {
+                if (connectionFromMap.containsKey(outNode.getNodeId())) {
+                    connectionFromMap.put(outNode.getNodeId(), new ArrayList<DistanceNodeTupel>());//adding a list instance so we can add it
+                }
+                connectionFromMap.get(outNode.getNodeId()).add(new DistanceNodeTupel(fromNode.getNodeId(), dist));
+                stack.add(outNode.getNodeId());
+            }
+            return false;
+        }
     }
-    
-    private int getMaxDistanceToThisNode(int fromId) {
+
+    private int getMaxDistanceToThisNode(final int fromId) {
         int max = 0;
         for (DistanceNodeTupel t : connectionFromMap.get(fromId)) {
-            if (t.distance > max) max = t.distance;
+            if (t.distance > max)
+                max = t.distance;
         }
         return max;
     }
-    
+
     private class DistanceNodeTupel {
         public int nodeId;
         public int distance;
+
         public DistanceNodeTupel(int nodeId, int distance) {
             this.nodeId = nodeId;
             this.distance = distance;
